53a765fd949ad0b5bc1b8e91dfd225c033e957b4
willbennett95 <will-bennett@hotmail.co.uk>
2019-05-20 09:42:04 +0100

    updated startup

--- startup.sh
+++ startup.sh
@@ -1,21 +1,26 @@
-!#/bin/bash
+#!/bin/bash
 
 # Installing git
-sudo yum install -y git
+yum install -y git
+
+USER="will4474"
 
 # Installing python
-sudo yum install gcc openssl-devel bzip2-devel
-sudo wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
-sudo tar xzf Python-3.7.2.tgz
-cd Python-3.7.2.tgz
-./configure ––enable–optimizations
-make altinstall
-sudo yum install python-pip
-sudo yum install python-devel
-sudo yum groupinstall 'development tools'
+
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+yum update -y
+yum install -y python36u python36u-libs python36u-devel python36u-pip
+alias python='python3.6'
+pip3.6 install --upgrade pip
+alias pip='pip3.6'
+pip install virtualenv
 
 # create project resources
-mkdir projects
-cd projects
-mkvirtualenv django
-pip install Django==1.5
+mkdir /home/$USER/projects
+su - $USER
+cd /home/$USER/projects
+virtualenv djangoenv
+source /home/$USER/projects/djangoenv/bin/activate
+sudo pip install django
+
+
